bb6c6b2f29897e5501974aa9d8dd1b1b9d4670d3
Sandhj <135236046+Sandhj@users.noreply.github.com>
2024-05-01 18:59:40 +0800

    Create A.sh

---
+++ A.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Buat direktori jika belum ada
+mkdir -p /root/
+cd /root/
+
+# Buat script untuk restart otomatis Xray
+cat << 'EOF' > nama.py
+
+ #ISI SCRIPT DSINI
+ 
+EOF
+
+# Berikan izin eksekusi pada script
+chmod +x x.py
+
+# Buat file unit systemd untuk menjalankan script
+cat << 'EOF' > /etc/systemd/system/nama.service
+[Unit]
+Description=San Store
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=/root/
+ExecStart=/usr/bin/python3 nama.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload konfigurasi systemd
+systemctl daemon-reload
+
+# Aktifkan dan mulai layanan
+systemctl enable x.service
+systemctl start x.service
+
+echo "Restart X-ray tiap 5 Menit Dimulai"
+
+cd
+rm namascript.sh
